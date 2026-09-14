2ca515f9bf699d169128b111a6e0c36634dbd653
officiallwebsite <targett100k@gmail.com>
2026-09-14 21:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-function goToPayment() {
+function buyNow(){
+
   window.location.href = "payment.html";
-}
 
-function copyUPI() {
-  const upi = document.getElementById("upiId").innerText.trim();
-
-  navigator.clipboard.writeText(upi).then(() => {
-    const btn = document.querySelector(".copy-btn");
-    btn.innerText = "✓ COPIED";
-
-    setTimeout(() => {
-      btn.innerText = "📋 COPY UPI ID";
-    }, 2000);
-  });
-}
-
-function paymentMessage() {
-  const message = document.getElementById("message");
-
-  message.style.display = "block";
-  message.innerHTML =
-    "⚠️ Payment verification required. Please complete the ₹99 payment first and keep your payment screenshot ready.";
 }
