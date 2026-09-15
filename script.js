f75bc70613391916af179e126af0365d8f985ac8
officiallwebsite <targett100k@gmail.com>
2026-09-15 06:18:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 function buyNow() {
   window.location.href = "payment.html";
 }
+
+
+function copyUPI() {
+
+  const upi = document
+    .getElementById("upiId")
+    .innerText
+    .trim();
+
+  navigator.clipboard.writeText(upi);
+
+  const message =
+    document.getElementById("copyMessage");
+
+  message.innerText = "✓ UPI ID COPIED";
+
+  setTimeout(function() {
+
+    message.innerText = "";
+
+  }, 2000);
+}
+
+
+function paymentDone() {
+
+  const message =
+    document.getElementById("paymentMessage");
+
+  message.style.display = "block";
+
+  message.innerHTML =
+    "⚠️ Please complete the ₹99 payment first. After payment, keep your payment screenshot ready for verification.";
+
+}
